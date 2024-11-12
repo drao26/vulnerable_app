52b04e5a770caced1e4f16773ca0c3d0ee86ba40
drao26 <150990454+drao26@users.noreply.github.com>
2024-11-12 13:03:15 +1100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-GITHUB_API_KEY = "ghp_ExampleSecretToken1234567890"
-SERVICE_API_KEY = "apikey_1234EXAMPLE5678"
-OAUTH_TOKEN = "oauth_12345_example_OAuth_token"
-DATABASE_PASSWORD = "ExamplePassword123!"
-JWT_SECRET = "examplejwtsecrethash"
+
